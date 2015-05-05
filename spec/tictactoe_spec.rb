138ec667399efb404ef83130613c7d8d00779f9f
Chris Foster <fosterchris22@gmail.com>
2015-05-05 17:26:32 -0400

    Refactor tests to include variables.

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,31 +2,34 @@ require 'rspec'
 require_relative '../game'
 
 RSpec.describe Game do
+  before(:each) do
+    @game = Game.new
+  end
 
   it 'takes a move' do
-    game = Game.new
-    game.move("M2")
-    expect(game.moves).to eql ["M2"]
+    @game.move("M2")
+    expect(@game.moves).to eql ["M2"]
   end
 
   it 'has two players' do
-    game = Game.new
-    expect(game.players.count).to eql 2
+    expect(@game.players.count).to eql 2
   end
 
   it 'has an AI player and a Human player' do
-    game = Game.new
-    expect(game.players.first.class).to eql Ai
-    expect(game.players.last.class).to eql Player
+    expect(@game.players.first.class).to eql Ai
+    expect(@game.players.last.class).to eql Player
   end
 
 end
 
 RSpec.describe Player do
 
+  before(:each) do
+    @game = Game.new
+  end
+
   it 'makes a move' do
-    game = Game.new
-    player = game.human
+    player = @game.human
     player.move("M2")
     expect(player.moves).to eql ["M2"]
   end
@@ -35,9 +38,12 @@ end
 
 RSpec.describe Ai do
 
+  before(:each) do
+    @game = Game.new
+  end
+
   it 'makes a move' do
-    game = Game.new
-    ai = game.ai
+    ai = @game.ai
     ai.move("M2")
     expect(ai.moves).to eql ["M2"]
   end
